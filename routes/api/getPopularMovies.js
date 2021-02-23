d5a16ccaf9fac0b506923307a570c283179dac11
Parker Catalano <catalano.work@gmail.com>
2021-02-22 21:13:39 -0500

    save

--- routes/api/getPopularMovies.js
+++ routes/api/getPopularMovies.js
@@ -3,3 +3,21 @@ const router = express.Router();
 const fetch = require("node-fetch");
 const dotenv = require("dotenv").config();
 const api_key = process.env.API_KEY;
+
+const getPopularMovies = async (page) => {
+    const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=en-US&page=${page}`);
+    const data = await response.json();
+    return data;
+};
+
+// @route GET api/getPopularMovies
+// @desc Retrieves popular movie titles from https://developers.themoviedb.org/3/movies/get-popular-movies
+router.get("/", (req, res) => {
+    const pageNumber = req.query.pagenumber;
+    console.log(`Fetching Movies for Page: ${pageNumber}`);
+    getPopularMovies(pageNumber).then(data => {
+        res.send({ response: data });
+    });
+});
+
+module.exports = router;
